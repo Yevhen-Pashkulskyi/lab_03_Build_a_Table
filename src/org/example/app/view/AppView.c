5806ccdc8dbc24254dcba82dcfd67021c0efea08
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-09-24 08:27:01 +0300

    save

--- src/org/example/app/view/AppView.c
+++ src/org/example/app/view/AppView.c
@@ -1,5 +1,12 @@
-//Pashkulskyi Yevhen AS-246
 #include <stdio.h>
-void menu() {
-    printf("\"");
+#include <stdlib.h>
+#include <string.h>
+
+#define HELLO_STRING "Hello, people!\n"
+
+void show_menu() {
+    char *str = malloc(sizeof(char) * (strlen(HELLO_STRING) + 1));
+    strcpy(str, HELLO_STRING);
+    printf("->\t%s", str);
+    free(str);
 }
